1bfd1a1e5f632351f9d9522342651bb7fdbac555
Pruthil Prajapati <sunnyprajapati99931@gmail.com>
2025-08-03 19:06:05 +0530

    Added Stochastic Gradient Descent Class

--- linear_regression/Gradient_Descent_regressor.py
+++ linear_regression/Gradient_Descent_regressor.py
@@ -43,3 +43,47 @@ Y_pred = model.predict(X_test)
 print(model.r2_score(Y_true,Y_pred))
 
 """
+
+"""
+This is Stochastic Gradient Descent Implementation 
+where for each epoch not whole data error is taken
+but only some random y's are taken in consideration
+
+"""
+class SGDRegressor:
+  def __init__(self, Learning_Rate, Epochs):
+    self.Learning_Rate = Learning_Rate
+    self.Epochs = Epochs
+    self.coef = None
+    self.intercept = None
+
+  def fit(self, X_train, Y_train):
+    self.intercept = 0
+    self.coef = np.ones(X_train.shape[1])
+
+    for i in range(self.Epochs):
+      for j in range(X_train.shape[0]):
+        idx = np.random.randint(0,X_train.shape[0])
+
+        Y_hat = np.dot(X_train[idx], self.coef) + self.intercept
+        intercept_der = -2 * (Y_train[idx] - Y_hat)
+        self.intercept -= self.Learning_Rate * intercept_der
+        coef_der = -2 * np.dot((Y_train[idx] - Y_hat), X_train[idx])
+        self.coef -= self.Learning_Rate * coef_der
+
+  def predict(self, X_test):
+    if self.coef is not None:
+        return np.dot(X_test, self.coef) + self.intercept
+
+  def r2_score(self, Y_true, Y_pred):
+    ss_total = np.sum((Y_true - Y_true.mean()) ** 2)
+    ss_res = np.sum((Y_true - Y_pred) ** 2)
+    return 1 - (ss_res / ss_total)
+  
+model_stochastic = SGDRegressor(0.01,1000)
+
+"""
+model_stochastic.fit(X_train , Y_train)
+Y_pred = model_stochastic.predict(X_test)
+print(model_stochastic.r2_score(Y_true,Y_pred))
+"""
